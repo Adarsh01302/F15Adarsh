da2f1735f39b7e69be96c0a7ee6ff559929a9d0d
sdac17 <sdac17@sdac17-PC>
2023-04-05 16:53:47 +0530

    Update1.0.1

--- Githubdemo/src/Main2.java
+++ Githubdemo/src/Main2.java
@@ -1,6 +1,6 @@
 
 public class Main2 {
-public static void main(String[] args) {
-	System.out.println("update 2.0");
-}
+	public static void main(String[] args) {
+		System.out.println("Updated 1.0.1");
+	}
 }
